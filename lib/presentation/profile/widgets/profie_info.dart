dd0a8228bcee4a67aa62cb5029b7e50c532efa2d
Ara Periyan <periyan.ara@gmail.com>
2023-11-04 15:29:21 +0400

    add log out

--- lib/presentation/profile/widgets/profie_info.dart
+++ lib/presentation/profile/widgets/profie_info.dart
@@ -1,6 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_svg/svg.dart';
+import 'package:foundation_2/presentation/common/blocs/auth_bloc/auth_bloc.dart';
 import 'package:foundation_2/presentation/common/widgets/custom_rounded_button.dart';
+import 'package:foundation_2/presentation/sign_up/signup_screen.dart';
 import 'package:foundation_2/presentation/theme/extensions/app_profile_theme.dart';
 
 class ProfileInfo extends StatelessWidget {
@@ -11,30 +14,41 @@ class ProfileInfo extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Column(
-      children: [
-        const Padding(padding: EdgeInsets.only(top: 50)),
-        CircleAvatar(
-          radius: profileTheme.avatarBorderRadius,
-          child: SizedBox(
-            height: profileTheme.avatarHeightAndWidth,
-            width: profileTheme.avatarHeightAndWidth,
-            child: SvgPicture.asset('assets/images/people.svg'),
+    return BlocListener<AuthBloc, AuthState>(
+      listener: (context, state) {
+        if (state is AuthAuthenticatedState) {
+          MaterialPageRoute<void>(
+            builder: (BuildContext context) => const SignupScreen(),
+          );
+        }
+      },
+      child: Column(
+        children: [
+          const Padding(padding: EdgeInsets.only(top: 50)),
+          CircleAvatar(
+            radius: profileTheme.avatarBorderRadius,
+            child: SizedBox(
+              height: profileTheme.avatarHeightAndWidth,
+              width: profileTheme.avatarHeightAndWidth,
+              child: SvgPicture.asset('assets/images/people.svg'),
+            ),
           ),
-        ),
-        const Padding(padding: EdgeInsets.only(top: 20)),
-        Text(
-          name,
-          style: TextStyle(color: profileTheme.black, fontSize: profileTheme.profileNameFontSize),
-        ),
-        const Padding(padding: EdgeInsets.only(top: 100)),
-        CustomRoundedButton(
-          isExpanded: false,
-          backgroundColor: profileTheme.blue,
-          text: 'Log Out',
-          onPressed: () {},
-        )
-      ],
+          const Padding(padding: EdgeInsets.only(top: 20)),
+          Text(
+            name,
+            style: TextStyle(color: profileTheme.black, fontSize: profileTheme.profileNameFontSize),
+          ),
+          const Padding(padding: EdgeInsets.only(top: 100)),
+          CustomRoundedButton(
+            isExpanded: false,
+            backgroundColor: profileTheme.blue,
+            text: 'Log Out',
+            onPressed: () => context.read<AuthBloc>().add(
+                  const AuthEvent.logout(),
+                ),
+          )
+        ],
+      ),
     );
   }
 }
