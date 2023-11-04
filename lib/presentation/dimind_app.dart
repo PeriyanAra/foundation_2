0939ad19f0ccf2513072e3ad9bdfcbb223b42d8c
Ara Periyan <periyan.ara@gmail.com>
2023-11-04 14:50:51 +0400

    merge

--- lib/presentation/dimind_app.dart
+++ lib/presentation/dimind_app.dart
@@ -1,12 +1,15 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:foundation_2/app/app_theme_mode/app_theme_mode.dart';
+import 'package:foundation_2/core/di/di.dart';
 import 'package:foundation_2/presentation/common/blocs/auth_bloc/auth_bloc.dart';
 import 'package:foundation_2/presentation/log_in/login_screen.dart';
 import 'package:foundation_2/presentation/theme/dimind_theme.dart';
 
 class DimindApp extends StatefulWidget {
-  const DimindApp({super.key});
+  const DimindApp({
+    super.key,
+  });
 
   @override
   State<DimindApp> createState() => _DimindAppState();
@@ -16,7 +19,7 @@ class _DimindAppState extends State<DimindApp> {
   @override
   Widget build(BuildContext context) {
     return BlocProvider(
-      create: (context) => AuthBloc(authUseCase: di<AuthBloc>()),
+      create: (context) => AuthBloc(),
       child: MaterialApp(
         debugShowCheckedModeBanner: false,
         title: 'DiMind',
